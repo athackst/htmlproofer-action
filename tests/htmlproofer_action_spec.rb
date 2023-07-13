de375c1e86379686a7961492ff4ee66347eaabfe
Allison Thackston <allison@allisonthackston.com>
2023-07-12 22:42:44 -0700

    Add missing command options. (#61)

--- tests/htmlproofer_action_spec.rb
+++ tests/htmlproofer_action_spec.rb
@@ -11,10 +11,48 @@ end
 
 # rubocop: disable Metrics/BlockLength
 describe EnvOptions do
+  describe '#get_env' do
+    context 'when a single environment variable is set' do
+      before do
+        ENV['INPUT_NAME'] = 'John Doe'
+      end
+
+      after do
+        ENV.delete('INPUT_NAME')
+      end
+
+      it 'returns the value of the environment variable' do
+        expect(EnvOptions.get_env('NAME')).to eq('John Doe')
+      end
+    end
+
+    context 'when multiple environment variables are set' do
+      before do
+        ENV['INPUT_FIRST_NAME'] = 'John'
+        ENV['INPUT_LAST_NAME'] = 'Doe'
+      end
+
+      after do
+        ENV.delete('INPUT_FIRST_NAME')
+        ENV.delete('INPUT_LAST_NAME')
+      end
+
+      it 'returns the value of the first non-nil environment variable' do
+        expect(EnvOptions.get_env(%w[FIRST_NAME LAST_NAME])).to eq('John')
+      end
+    end
+
+    context 'when no environment variable is set' do
+      it 'returns nil' do
+        expect(EnvOptions.get_env('NAME')).to be_nil
+      end
+    end
+  end
+
   describe '#get_bool' do
     context 'when the environment variable is set to a truthy value' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return('true')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('true')
       end
 
       it 'returns true' do
@@ -24,7 +62,7 @@ describe EnvOptions do
 
     context 'when the environment variable is set to a falsy value' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return('false')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('false')
       end
 
       it 'returns false' do
@@ -34,7 +72,7 @@ describe EnvOptions do
 
     context 'when the environment variable is not set' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return(nil)
+        allow(EnvOptions).to receive(:get_env).with('test').and_return(nil)
       end
 
       it 'returns the fallback value' do
@@ -44,7 +82,7 @@ describe EnvOptions do
 
     context 'when the environment variable is an empty string' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return('')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('')
       end
 
       it 'returns the fallback value' do
@@ -53,24 +91,24 @@ describe EnvOptions do
     end
   end
 
-  describe '#get_name_if' do
+  describe '#return_value_if' do
     context 'when the environment variable is set to a truthy value' do
       before do
-        allow(EnvOptions).to receive(:get_bool).with('test', false).and_return(true)
+        allow(EnvOptions).to receive(:get_env).with('test').and_return(true)
       end
 
       it 'returns the provided return_name' do
-        expect(EnvOptions.get_name_if('test', false, 'CustomName')).to eq('CustomName')
+        expect(EnvOptions.return_value_if('test', false, 'CustomName')).to eq('CustomName')
       end
     end
 
     context 'when the environment variable is not set or set to a falsy value' do
       before do
-        allow(EnvOptions).to receive(:get_bool).with('test', false).and_return(false)
+        allow(EnvOptions).to receive(:get_env).with('test').and_return(false)
       end
 
       it 'returns an empty string' do
-        expect(EnvOptions.get_name_if('test', false, 'CustomName')).to eq('')
+        expect(EnvOptions.return_value_if('test', false, 'CustomName')).to eq('')
       end
     end
   end
@@ -78,7 +116,7 @@ describe EnvOptions do
   describe '#get_int' do
     context 'when the environment variable is set to a valid integer' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return('42')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('42')
       end
 
       it 'returns the parsed integer' do
@@ -88,7 +126,7 @@ describe EnvOptions do
 
     context 'when the environment variable is not set' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return(nil)
+        allow(EnvOptions).to receive(:get_env).with('test').and_return(nil)
       end
 
       it 'returns the fallback value' do
@@ -98,7 +136,7 @@ describe EnvOptions do
 
     context 'when the environment variable is set to an invalid integer' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return('invalid')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('invalid')
       end
 
       it 'returns the fallback value' do
@@ -110,7 +148,7 @@ describe EnvOptions do
   describe '#get_str' do
     context 'when the environment variable is set' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return('hello')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('hello')
       end
 
       it 'returns the value of the environment variable as a string' do
@@ -120,7 +158,7 @@ describe EnvOptions do
 
     context 'when the environment variable is not set' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return(nil)
+        allow(EnvOptions).to receive(:get_env).with('test').and_return(nil)
       end
 
       it 'returns an empty string' do
@@ -130,7 +168,7 @@ describe EnvOptions do
 
     context 'when the environment variable is set to an empty string' do
       before do
-        allow(ENV).to receive(:[]).with('INPUT_test').and_return('')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('')
       end
 
       it 'returns an empty string' do
@@ -141,31 +179,76 @@ describe EnvOptions do
 
   describe '#get_list' do
     context 'when the value is a comma-separated string' do
+      before do
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('item1,item2,item3')
+      end
       it 'returns an array containing the items' do
-        value = 'item1,item2,item3'
-        expect(EnvOptions.get_list(value)).to eq(%w[item1 item2 item3])
+        expect(EnvOptions.get_list('test')).to eq(%w[item1 item2 item3])
       end
     end
 
     context 'when the value is a newline-separated string' do
+      before do
+        allow(EnvOptions).to receive(:get_env).with('test').and_return("item1\nitem2\nitem3")
+      end
       it 'returns an array containing the items' do
-        value = "item1\nitem2\nitem3"
-        expect(EnvOptions.get_list(value)).to eq(%w[item1 item2 item3])
+        expect(EnvOptions.get_list('test')).to eq(%w[item1 item2 item3])
       end
     end
 
     context 'when the value is a combination of comma-separated and newline-separated strings' do
+      before do
+        allow(EnvOptions).to receive(:get_env).with('test').and_return("item1,item2\nitem3")
+      end
       it 'returns an array containing all the items' do
-        value = "item1,item2\nitem3"
-        expect(EnvOptions.get_list(value)).to eq(%w[item1 item2 item3])
+        expect(EnvOptions.get_list('test')).to eq(%w[item1 item2 item3])
       end
     end
 
     context 'when the value is an empty string' do
-      it 'returns an empty array' do
-        value = ''
-        expect(EnvOptions.get_list(value)).to eq([])
+      before do
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('')
       end
+      it 'returns an empty array' do
+        expect(EnvOptions.get_list('test')).to eq([])
+      end
+    end
+  end
+
+  describe '#get_int_list' do
+    it 'returns an empty array when the environment variable is not set' do
+      expect(EnvOptions.get_int_list('test')).to eq([])
+    end
+
+    it 'returns an empty array when the environment variable is empty' do
+      allow(EnvOptions).to receive(:get_env).with('test').and_return('')
+
+      expect(EnvOptions.get_int_list('test')).to eq([])
+    end
+
+    it 'returns a list of integers when the environment variable contains comma-separated values' do
+      allow(EnvOptions).to receive(:get_env).with('test').and_return('1,2,3,4,5')
+
+      expect(EnvOptions.get_int_list('test')).to eq([1, 2, 3, 4, 5])
+    end
+
+    it 'returns a list of integers when the environment variable contains newline-separated values' do
+      allow(EnvOptions).to receive(:get_env).with('test').and_return("10\n20\n30\n40\n50")
+
+      expect(EnvOptions.get_int_list('test')).to eq([10, 20, 30, 40, 50])
+    end
+    # rubocop: disable Layout/LineLength
+    it 'returns a list of integers when the environment variable contains a combination of comma and newline separated values' do
+      allow(EnvOptions).to receive(:get_env).with('test').and_return("100,200\n300,400\n500")
+
+      expect(EnvOptions.get_int_list('test')).to eq([100, 200, 300, 400, 500])
+    end
+    # rubocop: enable Layout/LineLength
+
+    it 'returns the fallback value when the environment variable is not set' do
+      fallback = [1, 2, 3]
+
+      expect(EnvOptions.get_int_list('test', fallback)).to eq(fallback)
     end
   end
 
@@ -189,22 +272,11 @@ describe EnvOptions do
   describe '#get_regex_list' do
     context 'when the environment variable is set with comma-separated values' do
       before do
-        allow(EnvOptions).to receive(:get_str).with('test').and_return('/stackoverflow\.com/,http://github.com')
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('/stackoverflow\.com/,http://github.com')
       end
 
-      it 'returns an array of regular expressions when as_regex is true' do
-        result = EnvOptions.get_regex_list('test', true)
-        expect(result).to be_an(Array)
-        expect(result.size).to eq(2)
-        expect(result[0]).to be_a(Regexp)
-        expect(regex_or_substring_match('/stackoverflow.com/', result[0])).to be_truthy
-        expect(result[1]).to be_a(Regexp)
-        expect(regex_or_substring_match('http://github.com', result[1])).to be_truthy
-        expect(regex_or_substring_match('http://github.com/', result[1])).to be_truthy # Can match part of string
-      end
-
-      it 'returns an array of strings when as_regex is false' do
-        result = EnvOptions.get_regex_list('test', false)
+      it 'returns an array of strings and regular expressions ' do
+        result = EnvOptions.get_regex_list('test')
         expect(result).to be_an(Array)
         expect(result.size).to eq(2)
         expect(result[0]).to be_a(Regexp)
@@ -217,11 +289,42 @@ describe EnvOptions do
 
     context 'when the environment variable is not set' do
       before do
-        allow(EnvOptions).to receive(:get_str).with('test').and_return(nil)
+        allow(EnvOptions).to receive(:get_env).with('test').and_return(nil)
       end
 
       it 'returns an empty array' do
-        result = EnvOptions.get_regex_list('test', true)
+        result = EnvOptions.get_regex_list('test')
+        expect(result).to be_an(Array)
+        expect(result).to be_empty
+      end
+    end
+  end
+
+  describe '#get_only_regex_list' do
+    context 'when the environment variable is set with comma-separated values' do
+      before do
+        allow(EnvOptions).to receive(:get_env).with('test').and_return('stackoverflow\.com,http://github.com')
+      end
+
+      it 'returns an array of strings and regular expressions ' do
+        result = EnvOptions.get_only_regex_list('test')
+        expect(result).to be_an(Array)
+        expect(result.size).to eq(2)
+        expect(result[0]).to be_a(Regexp)
+        expect(regex_or_substring_match('http://stackoverflow.com', result[0])).to be_truthy
+        expect(result[1]).to be_a(Regexp)
+        expect(regex_or_substring_match('http://github.com', result[1])).to be_truthy
+        expect(regex_or_substring_match('http://github.com/', result[1])).to be_truthy # Can match substring
+      end
+    end
+
+    context 'when the environment variable is not set' do
+      before do
+        allow(EnvOptions).to receive(:get_env).with('test').and_return(nil)
+      end
+
+      it 'returns an empty array' do
+        result = EnvOptions.get_only_regex_list('test')
         expect(result).to be_an(Array)
         expect(result).to be_empty
       end
@@ -230,9 +333,9 @@ describe EnvOptions do
 
   describe '#get_swap_map' do
     it 'returns a hash with regex expression => string replacement with newline' do
-      allow(EnvOptions).to receive(:get_str).with('URL_SWAP').and_return("foo:bar\nbaz:qux")
+      allow(EnvOptions).to receive(:get_env).with('test').and_return("foo:bar\nbaz:qux")
 
-      result = EnvOptions.get_swap_map('URL_SWAP')
+      result = EnvOptions.get_swap_map('test')
 
       expect(result).to be_a(Hash)
       expect(result.size).to eq(2)
@@ -241,9 +344,9 @@ describe EnvOptions do
     end
 
     it 'returns a hash with regex expression => string replacement with commas' do
-      allow(EnvOptions).to receive(:get_str).with('URL_SWAP').and_return('foo:bar,baz:qux')
+      allow(EnvOptions).to receive(:get_env).with('test').and_return('foo:bar,baz:qux')
 
-      result = EnvOptions.get_swap_map('URL_SWAP')
+      result = EnvOptions.get_swap_map('test')
 
       expect(result).to be_a(Hash)
       expect(result.size).to eq(2)
@@ -252,9 +355,9 @@ describe EnvOptions do
     end
 
     it 'handles escaped colons correctly' do
-      allow(EnvOptions).to receive(:get_str).with('URL_SWAP').and_return('colon\\:bar:escape\\:qux')
+      allow(EnvOptions).to receive(:get_env).with('test').and_return('colon\\:bar:escape\\:qux')
 
-      result = EnvOptions.get_swap_map('URL_SWAP')
+      result = EnvOptions.get_swap_map('test')
 
       expect(result).to be_a(Hash)
       expect(result.size).to eq(1)
@@ -306,8 +409,9 @@ describe HtmlprooferAction do
       allow(EnvOptions).to receive(:get_bool).and_return(true)
       allow(EnvOptions).to receive(:get_int).and_return(50, 30, 0, 120)
       allow(EnvOptions).to receive(:get_str).and_return('/site')
-      allow(EnvOptions).to receive(:get_name_if).and_return('Favicon', 'Links', 'Images', 'Scripts', 'OpenGraph')
-      allow(EnvOptions).to receive(:get_regex_list).and_return([], [], [], ['404.html', Regexp.new('foo')])
+      allow(EnvOptions).to receive(:return_value_if).and_return('Favicon', 'Links', 'Images', 'Scripts', 'OpenGraph')
+      allow(EnvOptions).to receive(:get_regex_list).and_return(['404.html', Regexp.new('foo')], [])
+      allow(EnvOptions).to receive(:get_only_regex_list).and_return([])
       allow(EnvOptions).to receive(:get_swap_map).and_return({
                                                                Regexp.new('foo') => 'bar',
                                                                Regexp.new('baz') => 'qux'
@@ -331,12 +435,12 @@ describe HtmlprooferAction do
       expect(result[:typhoeus][:timeout]).to eq(120)
       expect(result[:typhoeus][:cookiefile]).to eq('.cookies')
       expect(result[:typhoeus][:cookiejar]).to eq('.cookies')
+      expect(result[:ignore_files]).to eq(['404.html', Regexp.new('foo')])
       expect(result[:ignore_urls]).to eq([])
       expect(result[:swap_urls]).to eq({
                                          Regexp.new('foo') => 'bar',
                                          Regexp.new('baz') => 'qux'
                                        })
-      expect(result[:ignore_files]).to eq(['404.html', Regexp.new('foo')])
     end
   end
 end
