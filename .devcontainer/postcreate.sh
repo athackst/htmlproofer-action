033013b833e7c145c15cba99de06aa2dd5c9b6cf
Allison Thackston <allison@allisonthackston.com>
2026-01-21 22:00:00 +0000

    Change action to composite and create cache by default

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+python3 -m venv .venv
+source .venv/bin/activate
+pip install mkdocs mkdocs-simple-plugin mkdocs-material
+echo "source $(pwd)/.venv/bin/activate" >> ~/.bashrc
+
+cd htmlproofer
+
+# Detect Ruby version
+RUBY_VERSION=$(ruby -v | awk '{print $2}')
+echo "$RUBY_VERSION" > .ruby-version
+
 # Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
     echo "Installing bundler in gemfile"
@@ -11,22 +22,3 @@ fi
 if [ -f Gemfile ]; then
     bundle install
 fi
-
-python3 -m venv .venv
-source .venv/bin/activate
-pip install mkdocs mkdocs-simple-plugin mkdocs-material
-echo "source $(pwd)/.venv/bin/activate" >> ~/.bashrc
-
-# Detect Ruby version
-RUBY_VERSION=$(ruby -v | awk '{print $2}')
-
-# Create or update .tool-versions file
-if grep -q "^ruby " .tool-versions 2>/dev/null; then
-  # Update existing entry
-  sed -i.bak "s/^ruby .*/ruby $RUBY_VERSION/" .tool-versions
-else
-  # Append new entry
-  echo "ruby $RUBY_VERSION" >> .tool-versions
-fi
-
-echo ".tool-versions updated with ruby $RUBY_VERSION"
